b9df45528a5bf54c4c287ef927e9279ba018cb37
tejapamuru4111 <pamurutejayadav@gmail.com>
2023-02-17 10:47:50 +0530

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from './components/Home'
+import BookView from './components/BookView';
+import AddBook from './components/AddBook';
+import NotFound from './components/NotFound';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+       <Route exact path='/' element={<Home />} />
+       <Route exact path='/add-new-book' element={<AddBook />} />
+       <Route exact path='/book-view/:id' element={<BookView />} />
+       <Route exact path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App;
